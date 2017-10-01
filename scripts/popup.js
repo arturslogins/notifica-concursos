61193f67819679b0d97519fe9b293c59803bc1e7
Romildo Oliveira Souza Júnior <contact@mildo.me>
2017-09-30 22:29:46 -0300

    Added parseData function

--- scripts/popup.js
+++ scripts/popup.js
@@ -8,10 +8,36 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
     'https://www.pciconcursos.com.br/concursos/nordeste/',
   ];
 
+  var tenders = [];
+
+  function parseData(data) {
+    var tendersData = $('#concursos', data).children();
+    var region;
+
+    tendersData.each(function(index, tenderData) {
+      if (tenderData.className == 'ua') {
+        region = tenderData.id;
+      } else if (
+          tenderData.firstElementChild != null &&
+          tenderData.firstElementChild.className == 'ca') {
+        tenderData = tenderData.firstElementChild;
+        var tender = {
+          'title': tenderData.firstElementChild.text,
+          'vacancies': parseInt(tenderData.getElementsByClassName('cd')[0]
+                                    .firstChild.textContent.match(/\d+ vaga/)),
+          'region': region,
+          'url': tenderData.firstElementChild.href
+        };
+        tenders.push(tender);
+      }
+    });
+  }
+
   function getData(url) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status === 200) {
+        parseData(xhr.response);
       }
     };
     xhr.open('GET', url, true);
